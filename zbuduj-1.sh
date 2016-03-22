8c3aa9c2e789eb9f37211906364334680f258205
sekollokes <tomasz@sekscinski.pl>
2016-03-22 15:16:16 +0100

    initial

---
+++ zbuduj-1.sh
@@ -0,0 +1,62 @@
+echo binutils-stage1-pass1
+./binutils-stage1-pass1
+echo gcc-stage1-pass1
+./gcc-stage1-pass1
+echo linux-api-headers-stage1
+./linux-api-headers-stage1
+echo ./glibc-stage1
+./glibc-stage1
+echo ./libstdc-stage1
+./libstdc-stage1
+echo ./binutils-stage1-pass2
+./binutils-stage1-pass2
+echo ./gcc-stage1-pass2
+./gcc-stage1-pass2
+echo ./tcl-core-stage1
+./tcl-core-stage1
+echo ./expect-stage1
+./expect-stage1
+echo ./dejagnu-stage1
+./dejagnu-stage1
+echo ./check-stage1
+./check-stage1
+echo ./ncurses-stage1
+./ncurses-stage1
+echo ./bash-stage1
+./bash-stage1
+echo ./bzip2-stage1
+./bzip2-stage1
+echo ./coreutils-stage1
+./coreutils-stage1
+echo ./diffutils-stage1
+./diffutils-stage1
+echo ./file-stage1
+./file-stage1
+echo ./findutils-stage1
+./findutils-stage1
+echo ./gawk-stage1
+./gawk-stage1
+echo ./gettext-stage1
+./gettext-stage1
+echo ./grep-stage1
+./grep-stage1
+echo ./gzip-stage1
+./gzip-stage1
+echo ./m4-stage1
+./m4-stage1
+echo ./make-stage1
+./make-stage1
+echo ./patch-stage1
+./patch-stage1
+echo ./perl-stage1
+./perl-stage1
+echo ./sed-stage1
+./sed-stage1
+echo ./tar-stage1
+./tar-stage1
+echo ./texinfo-stage1
+./texinfo-stage1
+echo ./util-linux-stage1
+./util-linux-stage1
+echo ./xz-stage1
+./xz-stage1
